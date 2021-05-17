826dc38cb54ce070aee67b80dbca789e76b0af47
saidulislam <saidul.islam@gmail.com>
2021-05-17 08:11:53 -0400

    added more logging

--- main.py
+++ main.py
@@ -132,6 +132,12 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             vote2 = r.get(button2).decode('utf-8')
 
+            properties = {'custom_dimensions': {'Cats Vote': vote1}}
+            logger.warning("Vote cats", extra=properties)
+
+            properties = {'custom_dimensions': {'Dogs Vote': vote2}}
+            logger.warning("Vote dogs", extra=properties)
+
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
